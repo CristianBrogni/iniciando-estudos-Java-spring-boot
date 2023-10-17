3ed040951c62b3d9af8d2efb339acda2c0d81eaa
Cristian <cristianbrogni_28@hotmail.com>
2023-10-16 21:48:48 -0300

    Trabalhando com persistencia de dados, tratamento de repetibilidade de cadastro

--- src/main/java/br/com/cristianbrogni/todolist/user/UserController.java
+++ src/main/java/br/com/cristianbrogni/todolist/user/UserController.java
@@ -1,16 +1,27 @@
 package br.com.cristianbrogni.todolist.user;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
+    @Autowired
+    private IUserRepository userRepository;
+
     @PostMapping("/")
-    public void create(@RequestBody UserModel userModel) {
-        System.out.println(userModel.name);
+    public ResponseEntity create(@RequestBody UserModel userModel) {
+        var user = this.userRepository.findByUsername(userModel.getUsername());
+
+        if (user != null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuario ja existe");
+        }
+        var userCreated = this.userRepository.save(userModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
 }
